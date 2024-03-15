7b831525ffdbb411154caef0132c3f57e4c347f0
Niklas Pelli <niklaspelli22@gmail.com>
2024-03-16 00:29:35 +0100

    first commit, fixed API

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import Item from './components/Item';
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1>Den här är från App.jsx</h1>
+     <Item />
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
---
+++ src/backend/db.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { createConnection } from "mysql";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const database = createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "webshop",
+});
+
+database.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("MySQL connected!");
+  }
+});
+
+app.get("/data", (req, res) => {
+  database.query("SELECT * FROM id", (err, rows, fields) => {
+    if (!err) {
+      res.send(rows);
+    } else {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
