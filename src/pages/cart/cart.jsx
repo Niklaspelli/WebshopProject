17d20cd217db65259e2b459aa35ba7f4808ed0c5
Niklas Pelli <niklaspelli22@gmail.com>
2024-03-28 11:16:06 +0100

    fixat kundkorgen

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -4,13 +4,10 @@ import { CartItem } from './cart-item';
 import { useNavigate } from "react-router-dom";
 import "./cart.css";
 
-
 export const Cart = () => {
-  const [products, setProducts] = useState([]);
+  const [cartProducts, setCartProducts] = useState([]);
   const { cartItems, getTotalCartAmount } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
-
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -18,12 +15,14 @@ export const Cart = () => {
     fetch('http://localhost:3500/PRODUCTS')
       .then(res => res.json())
       .then(products => {
-        setProducts(products);
+        // Filter products to only include those in the cart
+        const productsInCart = products.filter(product => cartItems[product.id] > 0);
+        setCartProducts(productsInCart);
       })
       .catch(error => {
         console.error('Error fetching data: ', error);
       });
-  }, []);
+  }, [cartItems]);
 
   return (
     <div className="cart">
@@ -31,26 +30,23 @@ export const Cart = () => {
         <h1>Din Varukorg:</h1>
       </div>
       <div className="cartItems">
-        {products.map((product) => {
-          if (cartItems[product.id] !== 0) {
-            return <CartItem data={product} key={product.id}/>;
-          }
-         
-        })}
+        {cartProducts.map(product => (
+          <CartItem data={product} key={product.id}/>
+        ))}
       </div>
       
-{totalAmount > 0 ? (
-<div className="checkout">
-  <p> Summa:  {totalAmount}:-</p>
-  <button onClick={() => navigate("/")}> Fortsätt Handla</button>
-  <button> Kassa</button>
-</div>
-) : (<div className="checkout">
-  <h1 className="emptyCart">Din varukorg är tom!</h1>
-  
-  <button onClick={() => navigate("/")}> Fortsätt Handla</button>
-  </div>
-)}
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Summa:  {totalAmount}:-</p>
+          <button onClick={() => navigate("/")}> Fortsätt Handla</button>
+          <button> Kassa</button>
+        </div>
+      ) : (
+        <div className="checkout">
+          <h1 className="emptyCart">Din varukorg är tom!</h1>
+          <button onClick={() => navigate("/")}> Fortsätt Handla</button>
+        </div>
+      )}
     </div>
   );
 }
