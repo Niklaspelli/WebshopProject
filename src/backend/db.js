bd18a7955e87230112cd65a6a3b00b86dd3f5e5d
Niklas Pelli <niklaspelli22@gmail.com>
2024-03-28 15:18:36 +0100

    fixed the cart mapping

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { createConnection } from "mysql";
 import bodyParser from "body-parser";
 import cors from "cors";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
@@ -9,7 +9,31 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const database = createConnection({
+const filePath = "../../data/db.json";
+
+// Route to handle GET request for JSON data
+app.get("/products", (req, res) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      res.status(500).send("Error reading file");
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData); // Send JSON response
+    } catch (error) {
+      console.error("Error parsing JSON data:", error);
+      res.status(500).send("Error parsing JSON data");
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+/* const database = createConnection({
   host: "localhost",
   user: "root",
   password: "",
@@ -33,8 +57,4 @@ app.get("/products", (req, res) => {
       res.status(500).send("Internal Server Error");
     }
   });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+}); */
